32a9e67b1027fe3b458bb12ce80ab5370f1eaf82
kushal752 <kushaldabbe752@gmail.com>
2022-07-17 21:54:49 +0530

    added delete product function

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -38,7 +38,8 @@ export const getProducts = async (dispatch) => {
 export const deleteProduct = async (id, dispatch) => {
   dispatch(deleteProductStart());
   try {
-    // const res = await userRequest.delete(`/products/${id}`);
+    const res = await userRequest.delete(`/products/${id}`);
+    console.log(res);
     dispatch(deleteProductSuccess(id));
   } catch (err) {
     dispatch(deleteProductFailure());
